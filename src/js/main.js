9b488fd890c181177dd612d754454c58f02f9ca2
Lindsey Adams <linzma87@gmail.com>
2016-10-12 21:06:52 -0400

    Etsy API testing

--- src/js/main.js
+++ src/js/main.js
@@ -1,50 +1,50 @@
-var dogObject = {
+// // petfinder user info
+// user: {
+//     'apiKey': '0832500a5ec7269caa8dc84a045e3995',
+//     'mdFive': '8bd6fd56b5645e676457ab9d8c952f4c' // for pet.find
+// },
+//
+// getDogInfo: function() {
+//     // pet search request to petfinder
+//     $.ajax({
+//         'method': 'GET',
+//         'url': 'http://api.petfinder.com/my.method?key=' + this.user.apiKey + '&arg1=pet.find&sig=' + this.user.mdFive,
+//         'data': {},
+//         'datatype': 'json',
+//         'headers': {
+//           'content-type': 'application/json'
+//         },
+//         'success': function(data) {
+//             console.log(data);
+//         },
+//         'error': this.handleError
+//     });
+// },
 
-    // // petfinder user info
-    // user: {
-    //     'apiKey': '0832500a5ec7269caa8dc84a045e3995',
-    //     'mdFive': '8bd6fd56b5645e676457ab9d8c952f4c' // for pet.find
-    // },
-    //
-    // getDogInfo: function() {
+
+//Twitter
+
+// user: {
+//     'apiKey': 'dq1zLTQysQzSHYIJKHrSa8fQh',
+//     'encode': 'ZHExekxUUXlzUXpTSFlJSktIclNhOGZRaDpUTzFLcFE0d1ZDRzUxeFVwWEY0cW9yVkxmNFVpMGJKWElIZ255VFQ0VHBXNGJuUjN4aw==', // for Twitter
+//     'token': null
+// },
+
+var dogObject = (function() {
+
+    // function getHashtag() {
     //     // pet search request to petfinder
     //     $.ajax({
     //         'method': 'GET',
-    //         'url': 'http://api.petfinder.com/my.method?key=' + this.user.apiKey + '&arg1=pet.find&sig=' + this.user.mdFive,
+    //         'url': 'https://api.twitter.com/1.1/search/tweets.json?q=%23' + 'dog',
     //         'data': {},
-    //         'datatype': 'json',
-    //         'headers': {
-    //           'content-type': 'application/json'
-    //         },
+    //         'datatype': 'jsonp',
     //         'success': function(data) {
     //             console.log(data);
     //         },
     //         'error': this.handleError
     //     });
-    // },
-
-
-    //Twitter
-
-    user: {
-        'apiKey': 'dq1zLTQysQzSHYIJKHrSa8fQh',
-        'encode': 'ZHExekxUUXlzUXpTSFlJSktIclNhOGZRaDpUTzFLcFE0d1ZDRzUxeFVwWEY0cW9yVkxmNFVpMGJKWElIZ255VFQ0VHBXNGJuUjN4aw==', // for Twitter
-        'token': null
-    },
-
-    getHashtagResults: function() {
-        // pet search request to petfinder
-        $.ajax({
-            'method': 'GET',
-            'url': 'https://api.twitter.com/1.1/search/tweets.json?q=%23' + 'dog',
-            'data': {},
-            'datatype': 'jsonp',
-            'success': function(data) {
-                console.log(data);
-            },
-            'error': this.handleError
-        });
-    },
+    // }
 
 
     // $.get('http://api.petfinder.com/auth.getToken?key=' + this.user.apiKey + '&sig=' + this.user.shaOne).then(function(response){
@@ -54,18 +54,34 @@ var dogObject = {
 
     // update url hash
 
-    updateHash: function(hash) {
+    function updateHash(hash) {
         window.location.hash = hash;
-    },
+    }
 
     // handle errors
 
-    handleError: function(errorObject, textStatus, error) {
+    function handleError(errorObject, textStatus, error) {
         $('#content').empty();
         console.log(errorObject);
         // populateErrors(errorObject.status);
-    },
+    }
 
-};
+    //etsy
+    function getEtsy() {
 
-dogObject.getHashtagResults();
+      var api_key = "qkz6z22t3gggikbdmjsx4x4k";
+      var terms = 'dogs';
+      var etsyURL = 'https://openapi.etsy.com/v2/listings/active.js?keywords=' + terms + '&limit=12&includes=Images:1&api_key=' + api_key;
+        $.ajax({
+            'method': 'GET',
+            'url': etsyURL,
+            'data': {},
+            'dataType': 'json',
+            'success': function(data) {
+                console.log(data);
+            },
+            'error': this.handleError
+        });
+    }
+  getEtsy();
+})();
