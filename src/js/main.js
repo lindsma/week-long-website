eb09c469115264a12562ef5a60dad0d85dba04ff
Lindsey Adams <linzma87@gmail.com>
2016-10-13 20:25:40 -0400

    Favicon color change

--- src/js/main.js
+++ src/js/main.js
@@ -1,59 +1,27 @@
-// // petfinder user info
-// user: {
-//     'apiKey': '0832500a5ec7269caa8dc84a045e3995',
-//     'mdFive': '8bd6fd56b5645e676457ab9d8c952f4c' // for pet.find
-// },
-//
-// getDogInfo: function() {
-//     // pet search request to petfinder
-//     $.ajax({
-//         'method': 'GET',
-//         'url': 'http://api.petfinder.com/my.method?key=' + this.user.apiKey + '&arg1=pet.find&sig=' + this.user.mdFive,
-//         'data': {},
-//         'datatype': 'json',
-//         'headers': {
-//           'content-type': 'application/json'
-//         },
-//         'success': function(data) {
-//             console.log(data);
-//         },
-//         'error': this.handleError
-//     });
-// },
-
-
-//Twitter
-
-// user: {
-//     'apiKey': 'dq1zLTQysQzSHYIJKHrSa8fQh',
-//     'encode': 'ZHExekxUUXlzUXpTSFlJSktIclNhOGZRaDpUTzFLcFE0d1ZDRzUxeFVwWEY0cW9yVkxmNFVpMGJKWElIZ255VFQ0VHBXNGJuUjN4aw==', // for Twitter
-//     'token': null
-// },
 
 var dogObject = (function() {
 
-    // function getHashtag() {
-    //     // pet search request to petfinder
-    //     $.ajax({
-    //         'method': 'GET',
-    //         'url': 'https://api.twitter.com/1.1/search/tweets.json?q=%23' + 'dog',
-    //         'data': {},
-    //         'datatype': 'json',
-    //         'success': function(data) {
-    //             console.log(data);
-    //         },
-    //         'error': this.handleError
-    //     });
-    // }
-
-
-    // $.get('http://api.petfinder.com/auth.getToken?key=' + this.user.apiKey + '&sig=' + this.user.shaOne).then(function(response){
-    //   console.log(response);
-    // });
-    // },
-
+  function getPetfinder() {
+      // petfinder user info
+    var apiKey = '0832500a5ec7269caa8dc84a045e3995';
+    var mdFive = '8bd6fd56b5645e676457ab9d8c952f4c'; // for pet.find
+    var searchTerms = 'durham';
+      // pet search request to petfinder
+      $.ajax({
+          'method': 'GET',
+          'url': 'http://api.petfinder.com/pet.find?key=' + apiKey + '&location=' + searchTerms + '&breed=' + 'boxer' + '&sig=' + mdFive,
+          'data': {},
+          'dataType': 'jsonp',
+          'headers': {
+            'content-type': 'application/json'
+          },
+          'success': function(data) {
+              console.log(data);
+          },
+          'error': this.handleError
+      });
+  }
     // update url hash
-
     function updateHash(hash) {
         window.location.hash = hash;
     }
@@ -68,7 +36,6 @@ var dogObject = (function() {
 
     //etsy
     function getEtsy() {
-
       var api_key = "qkz6z22t3gggikbdmjsx4x4k";
       var terms = 'bed';
       var etsyURL = 'https://openapi.etsy.com/v2/listings/active.js?keywords=dog ' + terms + '&limit=12&includes=Images:1&api_key=' + api_key;
@@ -83,4 +50,5 @@ var dogObject = (function() {
         });
     }
   getEtsy();
+  // getPetfinder();
 })();
